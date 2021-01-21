32eaf7b94f1ca78ce289eb8e3dd9787f55d03f83
yujo <yujo@student.42seoul.kr>
2021-01-21 22:55:41 +0900

    pair programming 2

--- src/components/todoEdit.js
+++ src/components/todoEdit.js
@@ -1,9 +1,19 @@
-export const todoEdit = () => {
-    const $todoList = document.querySelector("#todo-list");
-    $todoList.addEventListener("dblclick", addClass);
-    
-}
+const addClass = ({ target }) => {
+  target.closest("li").classList.add("editing");
+  const $edit = target.closest("li").querySelector(".edit");
+  const targetLabel = target;
 
-const addClass = (event) => {
-    event.target.closest('li').classList.add('editing');
-}
+  $edit.addEventListener("keyup", ({ target, key }) => {
+    if (key === "Escape") {
+      target.closest("li").classList.remove("editing");
+    } else if (key === "Enter") {
+      targetLabel.innerText = target.value;
+      target.closest("li").classList.remove("editing");
+    }
+  });
+};
+
+export const todoEdit = () => {
+  const $todoList = document.querySelector("#todo-list");
+  $todoList.addEventListener("dblclick", addClass);
+};
